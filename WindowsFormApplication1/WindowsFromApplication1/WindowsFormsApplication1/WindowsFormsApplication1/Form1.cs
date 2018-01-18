8c7bddb10a6a834e7e7f74c0ddc07245b21f11f9
yain0 <31964478+yain0@users.noreply.github.com>
2018-01-18 12:04:39 +0400

    Add files via upload

---
+++ WindowsFormApplication1/WindowsFromApplication1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+        Prichal prichal;
+        public Form1()
+        {
+            InitializeComponent();
+            prichal = new Prichal();
+            Draw();
+        }
+        /*private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            prichal.Draw(gr, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+
+        }*/
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            prichal.Draw(gr, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var boat = new Boat(100, 4, 1000, dialog.Color);
+                int place = prichal.PutBoatPrichal(boat);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var boat = new NewBoat(100, 4, 1000, dialog.Color, true, true, dialogDop.Color);
+                    int place = prichal.PutBoatPrichal(boat);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                var boat = prichal.GetBoatPrichal(Convert.ToInt32(maskedTextBox1.Text));
+
+                Bitmap bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                boat.setPosition(5, 5);
+                boat.drawBoat(gr);
+                pictureBox2.Image = bmp;
+                Draw();
+            }
+        }
+    }
+}
---
+++ WindowsFormApplication1/WindowsFromApplication1/WindowsFormsApplication1/WindowsFormsApplication1/NewBoat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    public class NewBoat : Boat
+    {
+        private bool vint;
+        private bool kabina;
+        private Color dopColor;
+        public NewBoat(int maxSpeed, int maxCountPassenger, double weight, Color color,
+            bool vint, bool kabina, Color dopColor) :
+            base(maxSpeed, maxCountPassenger, weight, color)
+        {
+            this.vint = vint;
+            this.kabina = kabina;
+            this.dopColor = dopColor;
+        }
+        protected override void drawLightBoat(Graphics g)
+        {
+            if (vint)
+            {
+                Pen pen3 = new Pen(Color.Blue, 4);
+                g.DrawLine(pen3, startPosX - 7, startPosY - 5, startPosX + 10, startPosY + 10);
+                g.DrawLine(pen3, startPosX - 7, startPosY + 25, startPosX + 10, startPosY + 10);
+                g.DrawLine(pen3, startPosX - 7, startPosY + 10, startPosX + 10, startPosY + 10);
+
+
+            }
+            if (kabina)
+            {
+                Pen pen4 = new Pen(dopColor, 4);
+                g.DrawRectangle(pen4, startPosX + 10, startPosY, 20, 20);
+
+
+            }
+            base.drawLightBoat(g);
+            //Pen pen = new Pen(ColorBody, 5);
+            // 
+
+            // 
+        }
+    }
+}
