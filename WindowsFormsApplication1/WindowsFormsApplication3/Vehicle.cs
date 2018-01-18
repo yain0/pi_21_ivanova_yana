929b6f8fc67cf4174a9a5057f7a9fbc566ed4fae
yain0 <31964478+yain0@users.noreply.github.com>
2018-01-18 05:08:22 +0400

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication3/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApplication3
+{
+    public abstract class Vehicle : Transport
+    {
+        protected int startPosX;
+        protected int startPosY;
+        protected int countPassengers;
+        public virtual int MaxCountPassengers { protected set; get; }
+        public virtual int MaxSpeed { protected set; get; }
+        public Color ColorBody { protected set; get; }
+        public virtual double Weight { protected set; get; }
+        public abstract void moveBoat(Graphics g);
+        public abstract void drawBoat(Graphics g);
+        public void setPosition(int x, int y)
+        {
+            startPosX = x;
+            startPosY = y;
+        }
+        public void loadPassenger(int count)
+        {
+            if (countPassengers + count < MaxCountPassengers)
+            {
+                countPassengers += count;
+            }
+        }
+        public int getPassenger()
+        {
+            int count = countPassengers;
+            countPassengers = 0;
+            return count;
+        }
+    }
+}
